87df81546d93d21c49b04a87eb75d980944a02fb
starlordamit <59335027+starlordamit@users.noreply.github.com>
2023-01-18 23:45:06 +0530

    Create test_leap_year.c

---
+++ test_leap_year.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+int main() {
+int year;
+scanf("%d",&year);
+if((year%4==0) && ((year%400==0) || (year%100!=0))){
+printf("leap year");}
+else{
+printf("not a leap year");
+} return 0;
+}
