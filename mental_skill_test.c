6d1733f0c8c180d05876ec337fdf47520dc3a001
starlordamit <59335027+starlordamit@users.noreply.github.com>
2023-01-25 15:28:04 +0530

    Create mental_skill_test.c

---
+++ mental_skill_test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b;
+    char sym;
+    scanf("%d %d",&a,&b);
+    scanf("%c",&sym);
+    
+    switch(sym){
+        case('+'):
+            printf("%d",abs(a+b));
+            break;
+        case('-'):
+            printf("%d",abs(a-b));
+            break;
+        case('*'):
+            printf("%d",a*b);
+            break;
+        case('/'):
+            printf("%d",a/b);
+            break;
+        case('%'):
+            printf("%d",a%b);
+            break;
+        default:
+            printf("Invalid Choice");
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
